a90e03fc61c7cb57b827a47eba2a2ce5ee64a8f7
Mohamed Aziz Ouannes <azizouannes45@gmail.com>
2025-05-28 18:13:59 +0100

    backup automation

--- backup.sh
+++ backup.sh
@@ -14,11 +14,10 @@ tar czf "$BACKUP_DIR/prometheus-config.tar.gz" -C ./prometheus .
 echo "Backing up Alertmanager config..."
 tar czf "$BACKUP_DIR/alertmanager-config.tar.gz" -C ./alertmanager .
 
-# If you added a Prometheus data volume:
-if docker volume ls | grep -q prometheus-data; then
-  echo "Backing up Prometheus data volume..."
-  docker run --rm -v monitoring-stack_prometheus-data:/data -v "$BACKUP_DIR":/backup busybox tar czf /backup/prometheus-data.tar.gz -C /data .
-fi
+
+echo "Backing up Prometheus data volume..."
+docker run --rm -v monitoring-stack_prometheus-data:/data -v "$BACKUP_DIR":/backup busybox tar czf /backup/prometheus-data.tar.gz -C /data .
+
 
 echo "Backup completed in $BACKUP_DIR"
 
